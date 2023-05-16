351f2dbbe8c8c716cdee66c73f48c47e51fef46b
root <root@Debian.myguest.virtualbox.org>
2023-05-16 11:52:31 -0500

    final project

--- server.js
+++ server.js
@@ -21,7 +21,9 @@ app.get('/', (req, res) => {
 app.post('/makecall', (req, res) => {
 
     const {number} = req.body;
-
+    
+    const accountSid = "AC6643f3cb9b3c54539d54955cd611cabd";
+    const authToken = "7d20a3ebb1f3bf01bfa386527de8b579";
 
     const client = require("twilio")(accountSid, authToken);
 
@@ -41,7 +43,8 @@ app.post('/sendEmail', (req, res) => {
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        
+	user: 'godrestaurant13@gmail.com',
+        pass: 'deekjduvulgzjink'        
       }
     });
   
